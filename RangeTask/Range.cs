3ae21416a5c1da3bcb495778bb2996543ac6b251
Марат Ибатуллин <maratibatullin1992@gmail.com>
2023-08-20 18:24:15 +0700

    1st iteration

---
+++ RangeTask/Range.cs
@@ -0,0 +1,110 @@
+﻿namespace Range
+{
+    internal class Range
+    {
+        public double[] Interval1 { get; set; }
+
+        public double[] Interval2 { get; set; }
+
+        public Range(double[] interval1, double[] interval2)
+        {
+            Interval1 = interval1;
+            Interval2 = interval2;
+        }
+
+        public double[] GetIntervalIntersection(double[] interval1, double[] interval2)
+        {
+            double[] intervalIntersection = new double[2];
+
+            if (interval1[1] <= interval2[0] || interval2[1] <= interval1[0])
+            {
+                return new double[1];
+            }
+
+            if (interval1[0] <= interval2[0])
+            {
+                intervalIntersection[0] = interval2[0];
+            }
+            else
+            {
+                intervalIntersection[0] = interval1[0];
+            }
+
+            if (interval1[1] >= interval2[1])
+            {
+                intervalIntersection[1] = interval2[1];
+            }
+            else
+            {
+                intervalIntersection[1] = interval1[1];
+            }
+
+            return intervalIntersection;
+        }
+
+        public double[] GetIntervalUnion(double[] interval1, double[] interval2)
+        {
+            if (interval1[1] <= interval2[0] || interval2[1] <= interval1[0])
+            {
+                return new double[4] { interval1[0], interval1[1], interval2[0], interval2[1] };
+            }
+
+            double[] intervalUnion = new double[2];
+
+            if (interval1[0] <= interval2[0])
+            {
+                intervalUnion[0] = interval1[0];
+            }
+            else
+            {
+                intervalUnion[0] = interval2[0];
+            }
+
+            if (interval1[1] >= interval2[1])
+            {
+                intervalUnion[1] = interval1[1];
+            }
+            else
+            {
+                intervalUnion[1] = interval2[1];
+            }
+
+            return intervalUnion;
+        }
+
+        public double[] GetIntervalDifference(double[] interval1, double[] interval2)
+        {
+            if (interval1[1] <= interval2[0] || interval2[1] <= interval1[0])
+            {
+                return new double[2] { interval1[0], interval1[1] };
+            }
+
+            if (interval1[0] >= interval2[0] && interval1[1] <= interval2[1])
+            {
+                return new double[1];
+            }
+
+            if (interval1[0] >= interval2[0] && interval1[1] > interval2[1])
+            {
+                return new double[2] { interval2[1], interval1[1] };
+            }
+
+            if (interval1[0] < interval2[0] && interval1[1] <= interval2[1])
+            {
+                return new double[2] { interval1[0], interval2[0] };
+            }
+
+            double[] intervalDifference = new double[4];
+
+            if (interval1[0] < interval2[0] && interval1[1] > interval2[1])
+            {
+                intervalDifference[0] = interval1[0];
+                intervalDifference[1] = interval2[0];
+                intervalDifference[2] = interval2[1];
+                intervalDifference[3] = interval1[1];
+            }
+
+            return intervalDifference;
+        }
+    }
+}
---
+++ RangeTask/RangeProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    internal class RangeProgram
+    {
+        static void Main(string[] args)
+        {
+            double[] interval1 = new double[2] { 13, 18 };
+            double[] interval2 = new double[2] { 9, 12 };
+
+            Range range = new Range(interval1, interval2);
+
+            Console.WriteLine("Интервал - пересечение двух интервалов: " + string.Join(" - ", range.GetIntervalIntersection(interval1, interval2)));
+
+            Console.WriteLine("Интервал - объединение двух интервалов: " + string.Join(" - ", range.GetIntervalUnion(interval1, interval2)));
+
+            Console.WriteLine("Интервал - разность двух интервалов: " + string.Join(" - ", range.GetIntervalDifference(interval1, interval2)));
+        }
+    }
+}
